fa55c26b6b22138cb3082e1a0a329bc35b7d96d5
softprops <d.tangren@gmail.com>
2011-01-02 18:45:51 -0500

    wrap twitter api call in try catch in case of rate limiting

--- src/main/scala/Twitter.scala
+++ src/main/scala/Twitter.scala
@@ -8,13 +8,21 @@ object Twitter extends JsonCached with Config {
   import oauth._
   import net.liftweb.json.JsonAST._
   import net.liftweb.json.JsonDSL._
+  import java.util.logging.Logger
+
+  private final val log = Logger.getLogger(getClass.getName)
 
   implicit def http = new dispatch.AppEngineHttp
 
   def tweets =
     cacheOr("tweets", "current") {
-      val twts = http(Search("#nescala") as(Consumer(property("twttr_consumer"), property("twttr_consumer_secret")),
+      val twts: List[dispatch.json.JsObject] = try {
+        http(Search("#nescala").as(Consumer(property("twttr_consumer"), property("twttr_consumer_secret")),
                                             Token(property("twttr_token"), property("twttr_token_secret"))))
+      } catch { case _ =>
+        log.warning("Exception occured in twitter search api call, this client may be pass its rate limit")
+        Nil
+      }
       val result: List[(Option[BigDecimal], Option[String], Option[String], Option[String])] = try { for {
         t <- twts
       } yield {
