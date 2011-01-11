dfc59c7588e0b5fc48ed176de90e586b200da0a9
Nathan Hamblen <nathan@technically.us>
2011-01-11 04:53:57 -0500

    all talks

--- src/main/scala/Poll.scala
+++ src/main/scala/Poll.scala
@@ -129,7 +129,90 @@ object Poll {
            Scala for a single project to about ten people using or
            having used Scala on several projects. I'll talk about the
            good, the bad, and the ugly of our experiences with using
-           Scala during the past three years.""") :: Nil
+           Scala during the past three years.""") ::
+    Entry("Christopher League", "Continuations and Other Functional Patterns",
+          """This will be a quick recap and continuation (ha!) of my
+          last talk to the NY Scala Enthusiasts, called
+          Monadologie. We'll explore a few elegant, useful, and fun
+          design patterns from functional programming, with plenty of
+          sample code in Scala.""") ::
+    Entry("Nathan Hamblen", "Building an HTTP streaming API with Scala",
+          """Architecture and design considerations of a live rsvp
+          activity stream for the Meetup API that uses RabbitMQ and a
+          Netty-Unfiltered server to broadcast messages to many open
+          connections using few threads.""") ::
+    Entry("Nermin Serifovic", "Scala Performance Considerations",
+          """It's a well known fact that Scala performance is
+          generally on par with Java. However, using certain language
+          constructs can make Scala programs run slow. The goal of
+          this talk is to consider trade-offs between code elegance
+          and performance.""") ::
+    Entry("Alexandre Bertails", "Scala and the Semantic Web",
+          """The FeDeRate project from W3C is a sandbox for the
+          RDB2RDF Working Group, aiming at exposing Relational
+          DataBases to the Semantic Web. In this talk, I propose to
+          demo how one can use Scala to interact with the Semantic Web
+          using FeDeRate. We will also discuss about the
+          implementation.""") ::
+    Entry("Runar Bjarnason", "A Quick Tour of Scalaz",
+          """Scalaz is an open-source library for Scala which provides
+          abstractions to facilitate pure functional programming. A
+          brief introduction to Scalaz's pure functional data types
+          and type classes, including Monoids, Monads, and all the
+          rest.""") ::
+    Entry("Runar Bjarnason", "The Guerrilla Guide to Pure Functional Programming",
+          """Everything is a function. Every data structure is really
+          a program. Every program is a single referentially
+          transparent expression. These and other claims are
+          substantiated. Leave your side-effects at the door (or at
+          least put them in a continuation).""") ::
+    Entry("Eugene Yokota", "XML databinding with scalaxb",
+          """This talk will be a quick tour of scalaxb, an XML
+          databinding tool for Scala that turns XML Schema definition
+          into case classes, its use of typeclasses, and restful API
+          using Unfiltered.""") ::
+    Entry("Viktor Klang", "Building loosely coupled and scalable systems using EDA",
+          """Event-driven Architecture (EDA) is a software
+          architecture pattern promoting the production, detection,
+          consumption of, and reaction to, events. This architectural
+          pattern may be applied by the design and implementation of
+          applications and systems which transmit events among loosely
+          coupled software components and services.""") ::
+    Entry("Max Afonov", "Persistence of Scala case classes into MongoDB",
+          """MongoDB and Scala are a pair of technologies capable of
+          delivering impressive results when used properly
+          together. Alas, currently available tools don't provide a
+          convenient, simple, fast, and maintainable way of persisting
+          Scala objects into MongoDB in a type-safe way. I've spent
+          the last 18 months exploring various approaches to this
+          problem, and in this talk will explain what dead ends I've
+          encountered along the way. To top it off, I'll present my
+          newest work in this field, Salat.""") ::
+    Entry("Mark Harrah", "sbt 0.9: Why, what, how?",
+          """An overview of the changes coming in sbt 0.9, as well as
+          the motivations and thinking behind them.""") ::
+    Entry("Dean Wampler", "How Scala Influenced Haskell",
+          """(or is it the other way around?... ;^) Scala borrows many
+          good ideas from Haskell, from syntactic elements to major
+          features. I'll discuss some of these influences and also
+          describe ways in which Scala and Haskell are different.""") ::
+    Entry("Dean Wampler", "The Design of the Scala 2.8 Collections",
+          """The collections library was significantly revised in
+          Scala 2.8. Some of the changes solved design problems, such
+          the desire to return new instances of the same input type,
+          rather than a common parent type. Other changes make it
+          easier for user's to create their own collections that
+          exploit reusable features. I'll describe several of these
+          changes, using examples.""") ::
+    Entry("Maxime Lévesque", "Presentation of Squeryl",
+          """Squeryl aims to be an ORM/DSL for talking with Databases
+          with minimum verbosity and maximum type safety. The
+          presentation will be a quick tour of the features, I will
+          then go over the design decisions and compromises that were
+          made in attempting to blend the Relationnal, FP, and OO
+          paradigms together, followed by a discussion on how implicit
+          conversions and the type system were used to enforce the
+          typing rules prescribed by SQL, via a type arythmetic.""") :: Nil
 }
 case class Entry(speaker: String, title: String, description: String)
 
