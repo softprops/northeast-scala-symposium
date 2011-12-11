d1d71898882162a932f69a2a54425af85eace07e
softprops <d.tangren@gmail.com>
2011-12-10 19:55:32 -0500

    try to resolve PORT via env before default

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -5,7 +5,7 @@ object Server {
   import unfiltered.filter._
   import unfiltered.response.ResponseString
   def main(args: Array[String]) {
-    Http(8080)
+    Http(Option(System.getenv("PORT")).getOrElse("8080").toInt)
     .resources(getClass().getResource("/www"))
     .filter(Planify {
       NESS.site orElse boston.Boston.site orElse nyc.Nyc.site
