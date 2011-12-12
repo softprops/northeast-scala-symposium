50ee86d94d332b059ea5382edd4ee4f8bd606dbd
softprops <d.tangren@gmail.com>
2011-12-12 01:38:39 -0500

    redis pooling seems a bit unstable, play it safe

--- src/main/scala/Storage.scala
+++ src/main/scala/Storage.scala
@@ -2,7 +2,8 @@ package nescala
 
 object Store extends Config {
   import com.redis._
-  private lazy val (auth, clients) = {
+
+  /*private lazy val (auth, clients) = {
     val URI = """^redis://(\w+):(\w+)@(.*):(\d{4}).*""".r
     val prop = property("REDISTOGO_URL")
     prop match {
@@ -10,11 +11,24 @@ object Store extends Config {
         (pass, new RedisClientPool(host, port.toInt))
       case mf => sys.error("malformed redis uri: %s" format mf)
     }
-  }
+  }*/
  
+  def configured[T](f: RedisClient => T) = {
+    val URI = """^redis://(\w+):(\w+)@(.*):(\d{4}).*""".r
+    val prop = property("REDISTOGO_URL")
+    prop match {
+      case URI(_, pass, host, port) =>
+        val c = new RedisClient(host, port.toInt)
+        c.auth(pass)
+        f(c)
+      case mf => sys.error("malformed redis uri: %s" format mf)
+    }
+  }
+
   def apply[T](f: RedisClient => T) =
-    clients.withClient { c =>
+    configured(f)
+    /*clients.withClient { c =>
       c.auth(auth)
       f(c)
-    }
+    }*/
 }
