922340e91b1f550fca95ded1df63832f5ffa9185
softprops <d.tangren@gmail.com>
2011-12-13 02:32:56 -0500

    jumping in the 'pool' a second time

--- src/main/scala/Storage.scala
+++ src/main/scala/Storage.scala
@@ -3,32 +3,31 @@ package nescala
 object Store extends Config {
   import com.redis._
 
-  /*private lazy val (auth, clients) = {
+  private lazy val (auth, clients) = {
     val URI = """^redis://(\w+):(\w+)@(.*):(\d{4}).*""".r
     val prop = property("REDISTOGO_URL")
     prop match {
       case URI(_, pass, host, port) =>
         (pass, new RedisClientPool(host, port.toInt))
-      case mf => sys.error("malformed redis uri: %s" format mf)
-    }
-  }*/
- 
-  def configured[T](f: RedisClient => T) = {
-    val URI = """^redis://(\w+):(\w+)@(.*):(\d{4}).*""".r
-    val prop = property("REDISTOGO_URL")
-    prop match {
-      case URI(_, pass, host, port) =>
-        val c = new RedisClient(host, port.toInt)
-        c.auth(pass)
-        f(c)
-      case mf => sys.error("malformed redis uri: %s" format mf)
+      case mf =>
+        sys.error("malformed redis uri: %s" format mf)
     }
   }
 
-  def apply[T](f: RedisClient => T) =
-    configured(f)
-    /*clients.withClient { c =>
-      c.auth(auth)
-      f(c)
-    }*/
+  def apply[T](f: RedisClient => T): T =
+    clients.withClient { c =>
+      // here we are attempting an authenticated
+      // request. we will retry after trying
+      // to reconnect, should a connection is dropped
+      def attempt: T = try {
+        c.auth(auth)
+        f(c)
+      } catch {
+        case e: com.redis.RedisConnectionException =>
+          println("redis fail!")
+          if(c.reconnect) attempt
+          else throw e
+      }
+      attempt
+    }
 }
