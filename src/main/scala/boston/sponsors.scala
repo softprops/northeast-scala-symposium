d0c703dafc184ee10e5c1eafa9ed00da960c5f31
Nathan Hamblen <nathan@technically.us>
2012-02-11 18:10:07 -0500

    meetup made our totes

--- src/main/scala/boston/sponsors.scala
+++ src/main/scala/boston/sponsors.scala
@@ -9,6 +9,8 @@ trait SponsorTemplate { self: Templates =>
       <div id="sponsors">
         <h1>A Little Help From Our Friends</h1>
         <p>If you get more than your money's worth at nescala, it's thanks to these guys.</p>
+        <a href="http://www.meetup.com/jobs/"><img src="/images/sponsors/meetup.png" /></a>
+        <p>Meetup made our stylish yet functional tote bags.</p>
         <a href="https://foursquare.com/jobs/"><img src="/images/sponsors/foursquare.png" /></a>
         <p>Foursquare is buying your drinks Saturday night.</p>
         <a href="http://www.ardentex.com/software.html"><img src="/images/sponsors/ardentex.png" /></a>
