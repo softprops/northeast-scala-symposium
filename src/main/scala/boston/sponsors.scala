34e1df9d88c4e28157e6cc292919c4d94827ddec
Nathan Hamblen <nathan@technically.us>
2012-02-16 21:22:43 -0500

    heroku sponsorship

--- src/main/scala/boston/sponsors.scala
+++ src/main/scala/boston/sponsors.scala
@@ -10,11 +10,13 @@ trait SponsorTemplate { self: Templates =>
         <h1>A Little Help From Our Friends</h1>
         <p>If you get more than your money's worth at nescala, it's thanks to these guys.</p>
         <a href="http://www.typesafe.com/"><img src="/images/sponsors/typesafe.png" /></a>
-        <p>Typesafe hired professionals to record the talks.</p>
+        <p>Typesafe is hiring professionals to record the talks.</p>
         <a href="http://www.meetup.com/jobs/"><img src="/images/sponsors/meetup.png" /></a>
-        <p>Meetup made our stylish yet functional tote bags.</p>
+        <p>Meetup is making our stylish yet functional tote bags.</p>
+        <a href="http://www.heroku.com/"><img src="/images/sponsors/heroku.png" /></a>
+        <p>Heroku will take you into a happy hour cloud on Friday.</p>
         <a href="https://foursquare.com/jobs/"><img src="/images/sponsors/foursquare.png" /></a>
-        <p>Foursquare is buying your drinks Saturday night.</p>
+        <p>Foursquare is checking you in to free drinks on Saturday night.</p>
         <a href="http://www.ardentex.com/software.html"><img src="/images/sponsors/ardentex.png" /></a>
       </div>
     )
