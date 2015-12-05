9c673b33b72051272eedb5ad990a1fdfcbd470c1
softprops <d.tangren@gmail.com>
2015-12-05 14:43:18 +0900

    add docker script

---
+++ docker-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker run \
+       --rm \
+       -it \
+       --name nescala \
+       -p 8080:8080 \
+       -v $HOME/.sbt:/sbt \
+       -v $HOME/.ivy2:/y2 \
+       -v $PWD:/project \
+       jehrhardt/sbt
